6a2187353cde801e34536304693d50fe1876ebc9
Gerben Mulder <gerbenmulder2508@gmail.com>
2023-08-18 01:03:33 +0200

    feat: missing update.py

--- update.py
+++ update.py
@@ -2,8 +2,13 @@ import urequests
 import hashlib
 import mip
 import config
+from esp32 import Partition
 
 def download_firmware():
+    for p in Partition.find(Partition.TYPE_APP):
+        print(p)
+    
+    
     firmware_res = urequests.get("http://eya-solutions.com/firmware.bin")
     firmware_hash = urequests.get("http://eya-solutions.com/firmware.sha256")
     firmware = firmware_res.content
